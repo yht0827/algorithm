ba85c318e3eaf9ffaa890369fd68b37e9bb43442
yht <35368554+yht0827@users.noreply.github.com>
2018-02-19 12:21:32 +0900

    Update 수 찾기.cpp

--- Binary Search/수 찾기.cpp
+++ Binary Search/수 찾기.cpp
@@ -47,3 +47,46 @@ int main()
 
 	return 0;
 }
+
+
+///////////////////////////////////////////////////
+/* binary_search STL을 이용해서 구현
+
+#include<cstdio>
+#include<algorithm>
+
+int arr[100001],N,M,num;
+using namespace std;
+
+int main()
+{
+	bool ans;
+
+	scanf("%d", &N);
+
+	for (int i = 1; i <=N; i++)
+		scanf("%d", &arr[i]);
+
+	sort(arr+1,arr+(N+1));
+
+	scanf("%d", &M);
+
+	for (int i = 1; i <= M; i++)
+	{
+		scanf("%d",&num);
+		
+		ans = binary_search(arr+1, arr+(N+1), num);
+	
+		if (ans == true)
+			printf("1\n");
+		else
+			printf("0\n");
+		
+	}
+
+	return 0;
+}
+
+*/
+///////////////////////////////////////////////
+
