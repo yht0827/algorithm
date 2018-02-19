674d2faff60a5bd0a79456c745ce197c52c8c314
yht <35368554+yht0827@users.noreply.github.com>
2018-02-19 12:08:37 +0900

    Create 수 찾기.cpp

---
+++ Binary Search/수 찾기.cpp
@@ -0,0 +1,49 @@
+#include<cstdio>
+#include<algorithm>
+
+int arr[100001],N,M,num;
+using namespace std;
+
+int solve(int a)
+{
+	int lo = 1, hi = N;
+	
+	while (hi-lo>1)
+	{
+		int mid = (lo + hi)/2;
+		
+		if(a>arr[mid])
+		{
+			lo=mid;
+		}
+		else if(a<arr[mid]){
+			hi = mid;
+		}
+		else if(a==arr[mid]){// mid = a
+			return 1;
+		}
+
+	}
+
+	return 0;
+}
+
+int main()
+{
+	scanf("%d", &N);
+
+	for (int i = 1; i <=N; i++)
+		scanf("%d", &arr[i]);
+
+	sort(arr+1,arr+(N+1));
+
+	scanf("%d", &M);
+
+	for (int i = 1; i <= M; i++)
+	{
+		scanf("%d",&num);
+		printf("%d\n",solve(num));
+	}
+
+	return 0;
+}
