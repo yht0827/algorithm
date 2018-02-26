2d1e8cc29bfe7764fd83d39c1897ee785b4a1ecc
yht <35368554+yht0827@users.noreply.github.com>
2018-02-26 15:14:05 +0900

    Update 스타트링크.cpp

--- BFS/스타트링크.cpp
+++ BFS/스타트링크.cpp
@@ -1,59 +1,53 @@
-#include <iostream>
-#include <queue>
-#include <algorithm>
-#include <vector>
-#include <string.h>
+#include<cstdio>
+#include<queue>
+#include<vector>
+#include<algorithm>
 
 using namespace std;
-
 int F, S, G, U, D;
 
-int stair[10000000];
-bool visited[1000000];
-queue<int> Q;
-
-void bfs()
+void startlink(int F,int S,int G,int U,int D)
 {
-    while(!Q.empty())
-    {
-        int curr=Q.front();
-        Q.pop();
-        if(curr==G)
-            return;
-        if(U+curr<=F && !visited[curr+U])
-        {
-            stair[curr + U] = stair[curr] + 1;
-            Q.push(curr + U);
-            visited[curr + U] = true;
-        }
-        
-        if (curr-D>=1 && !visited[curr-D])
-        {
-            stair[curr - D] = stair[curr] + 1;
-            Q.push(curr - D);
-            visited[curr - D] = true;
-        }
-    }
-}
+	vector<bool> visited(F + 1, false); // 방문 여부를 저장하는 배열
+	queue<int> Q;
+	Q.push(S);
+	visited[S] = true;
+	// 탐색 시작
+	int level = 0;
+	while (!Q.empty()) {
+		int qSize = Q.size();
+		for (int i = 0; i < qSize; i++) {
+			int curr = Q.front();
+			Q.pop();
+
+			if (G == curr)
+			{
+				printf("%d\n", level);
+				return;
+			}
+
+			if (curr + U <= F && !visited[curr + U])//위로올라가는 경우
+			{
+				visited[curr + U] = true;
+				Q.push(curr + U);
+			}
+			if (curr - D >0 && !visited[curr - D])//아래로 내려가는 경우
+			{
+					visited[curr - D] = true;
+					Q.push(curr - D);
+			}
+		}
+		level++;
+	}
+	printf("use the stairs\n");//못 찾을 경우
+};
+
 int main()
 {
-    cin>>F>>S>>G>>U>>D;
-    memset(visited, false, sizeof(visited));
-    memset(stair, 0, sizeof(stair));
-    if (S == G)
-    {
-        printf("%d\n",0);
-        return 0;
-    }
-    
 
-    visited[S]=true;
-    stair[S]=0;
-    Q.push(S);
-    
-    bfs();
-    if(!visited[G])
-        cout<<"use the stairs"<<endl;
-    else
-        cout<<stair[G]<<endl;
+	scanf("%d %d %d %d %d", &F, &S, &G, &U, &D);
+
+	startlink(F,S,G,U,D);
+
+	return 0;
 }
