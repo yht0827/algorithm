9b9a0b6a5ae8b2c6414e8c34a4663edc26cdc0c8
양희태 <yangheetae0827@naver.com>
2018-08-01 20:16:56 +0900

    Update 분해합.java

--- Brute-force Search/분해합.java
+++ Brute-force Search/분해합.java
@@ -1,1 +1,34 @@
+import java.util.Scanner;
 
+public class Main {
+
+	private static int N;
+	private static int sum;
+	private static boolean flag;
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		for(int i=1;i<N;i++){
+			sum=0;
+			if(N==brute(i)){
+				System.out.println(i);
+				flag=true;
+				break;
+			}
+		}
+		if(!flag){
+			System.out.println(0);
+		}
+	}
+	public static int brute(int num){
+		sum=num;
+		do{
+			sum+=num%10;
+			num=num/10;
+		}while(num>0);
+
+		return sum;
+	}
+}
